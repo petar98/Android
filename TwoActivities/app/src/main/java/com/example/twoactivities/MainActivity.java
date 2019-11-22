44471e52d54334001261a4dc03bfb674b00cd741
Yanakiev <pyanakie@visteon.com>
2019-11-22 16:01:14 +0200

    Added TwoActivities project

---
+++ TwoActivities/app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.twoactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    public  static  final String EXTRA_MESSAGE = "com.example.android.twoactivities.extra.MESSAGE";
+    private EditText mMessageEditText;
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mMessageEditText = findViewById(R.id.editText_main);
+    }
+
+    public void launchSecondActivity(View view) {
+
+        Log.d(LOG_TAG, "Button Send clicked!");
+        Intent intent = new Intent(this, SecondActivity.class);
+        String message = mMessageEditText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
