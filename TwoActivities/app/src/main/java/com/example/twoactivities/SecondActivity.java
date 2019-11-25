f773223ed64aa76e4807f605f4c67226d7da38f8
Yanakiev <pyanakie@visteon.com>
2019-11-25 14:39:10 +0200

    Second Activity Response implemented

--- TwoActivities/app/src/main/java/com/example/twoactivities/SecondActivity.java
+++ TwoActivities/app/src/main/java/com/example/twoactivities/SecondActivity.java
@@ -4,15 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.strictmode.IntentReceiverLeakedViolation;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG_SECOND = SecondActivity.class.getSimpleName();
+    public static final String EXTRA_REPLY = "com.example.twoactivities.extra.REPLY";
+    private EditText replyText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        replyText = (EditText) findViewById(R.id.editText_second);
 
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
@@ -21,5 +31,12 @@ public class SecondActivity extends AppCompatActivity {
     }
 
     public void returnReply(View view) {
+        String reply = replyText.getText().toString();
+
+        Log.d(LOG_TAG_SECOND, "Reply Button was clicked!");
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_REPLY, reply);
+        setResult(RESULT_OK, replyIntent);
+        finish();
     }
 }
