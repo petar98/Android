e9bea1d9c10893b77862e5a0823200a82a9770a0
Yanakiev <pyanakie@visteon.com>
2019-12-10 17:28:12 +0200

    Scorekeeper app with extracted styles and night mode theme added

---
+++ Scorekeeper/app/src/main/java/com/example/scorekeeper/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.scorekeeper;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int mScore1;
+    private int mScore2;
+
+    private TextView mScoreText1;
+    private TextView mScoreText2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mScoreText1 = findViewById(R.id.score_1);
+        mScoreText2 = findViewById(R.id.score_2);
+    }
+
+    public void decreaseScore(View view) {
+        int viewID = view.getId();
+        switch (viewID) {
+            case R.id.decreaseTeam1:
+                mScore1--;
+                mScoreText1.setText(String.valueOf(mScore1));
+                break;
+            case R.id.decreaseTeam2:
+                mScore2--;
+                mScoreText2.setText(String.valueOf(mScore2));
+                break;
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == R.id.night_mode) {
+            int nightMode = AppCompatDelegate.getDefaultNightMode();
+            if (nightMode == AppCompatDelegate.MODE_NIGHT_YES) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            }
+        }
+
+        recreate();
+
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        int nightMode = AppCompatDelegate.getDefaultNightMode();
+        if (nightMode == AppCompatDelegate.MODE_NIGHT_YES) {
+            menu.findItem(R.id.night_mode).setTitle(R.string.day_mode);
+        } else {
+            menu.findItem(R.id.night_mode).setTitle(R.string.night_mode);
+        }
+
+        return true;
+    }
+
+    public void increaseScore(View view) {
+        int viewID = view.getId();
+        switch (viewID) {
+            case R.id.increaseTeam1:
+                mScore1++;
+                mScoreText1.setText(String.valueOf(mScore1));
+                break;
+            case R.id.increaseTeam2:
+                mScore2++;
+                mScoreText2.setText(String.valueOf(mScore2));
+                break;
+        }
+    }
+}
