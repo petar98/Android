ff4ea3d614e3518c7641d7004914b6d574b65df2
petar98 <petaryanakiev.py@gmail.com>
2020-03-10 18:21:07 +0200

    WhoWroteThisBook application finished

--- Java/WhoWroteThisBook/app/src/main/java/com/example/whowrotethisbook/FetchBook.java
+++ Java/WhoWroteThisBook/app/src/main/java/com/example/whowrotethisbook/FetchBook.java
@@ -3,6 +3,11 @@ package com.example.whowrotethisbook;
 import android.os.AsyncTask;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.lang.ref.WeakReference;
 
 public class FetchBook extends AsyncTask<String, Void, String> {
@@ -17,11 +22,46 @@ public class FetchBook extends AsyncTask<String, Void, String> {
 
     @Override
     protected String doInBackground(String... strings) {
-        return null;
+        return NetworkUtils.getBookInfo(strings[0]);
     }
 
     @Override
     protected void onPostExecute(String s) {
         super.onPostExecute(s);
+
+        try {
+            JSONObject jsonObject = new JSONObject(s);
+            JSONArray itemsArray = jsonObject.getJSONArray("items");
+
+            int i = 0;
+            String title = null;
+            String authors = null;
+
+            while (i < itemsArray.length() && authors == null && title == null) {
+                JSONObject book = itemsArray.getJSONObject(i);
+                JSONObject volumeInfo = book.getJSONObject("volumeInfo");
+
+                try {
+                    title = volumeInfo.getString("title");
+                    authors = volumeInfo.getString("authors");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                ++i;
+            }
+
+            if (title != null && authors != null) {
+                titleText.get().setText(title);
+                authorText.get().setText(authors);
+            } else {
+                titleText.get().setText(R.string.no_results);
+                authorText.get().setText("");
+            }
+        } catch (JSONException e) {
+            titleText.get().setText(R.string.no_results);
+            authorText.get().setText("");
+            e.printStackTrace();
+        }
     }
 }
--- Java/WhoWroteThisBook/app/src/main/java/com/example/whowrotethisbook/MainActivity.java
+++ Java/WhoWroteThisBook/app/src/main/java/com/example/whowrotethisbook/MainActivity.java
@@ -1,17 +1,35 @@
 package com.example.whowrotethisbook;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String SAVED_INPUT = "savedInput";
+    private static final String SAVED_TITLE = "savedTitle";
+    private static final String SAVED_AUTHOR = "savedAuthor";
+
     private EditText bookInput;
     private TextView titleText;
     private TextView authorText;
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putString(SAVED_INPUT, bookInput.getText().toString());
+        outState.putString(SAVED_TITLE, titleText.getText().toString());
+        outState.putString(SAVED_AUTHOR, authorText.getText().toString());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +39,43 @@ public class MainActivity extends AppCompatActivity {
         bookInput = findViewById(R.id.bookInput);
         titleText = findViewById(R.id.titleText);
         authorText = findViewById(R.id.authorText);
+
+        if (savedInstanceState != null) {
+            this.bookInput.setText(savedInstanceState.getString(SAVED_INPUT));
+            this.titleText.setText(savedInstanceState.getString(SAVED_TITLE));
+            this.authorText.setText(savedInstanceState.getString(SAVED_AUTHOR));
+        }
     }
 
     public void searchBooks(View view) {
         String queryString = bookInput.getText().toString();
+
+        // hide the keyboard after pressing Search
+        InputMethodManager inputManager = (InputMethodManager)
+                getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        if (inputManager != null) {
+            inputManager.hideSoftInputFromWindow(view.getWindowToken(),
+                    InputMethodManager.HIDE_NOT_ALWAYS);
+        }
+
+        ConnectivityManager connectivityManager = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = null;
+        if (connectivityManager != null) {
+            networkInfo = connectivityManager.getActiveNetworkInfo();
+        }
+
+        if (networkInfo != null && networkInfo.isConnected() && queryString.length() != 0) {
+            new FetchBook(titleText, authorText).execute(queryString);
+            titleText.setText(R.string.loading);
+            authorText.setText("");
+        } else if (queryString.length() == 0) {
+            authorText.setText("");
+            titleText.setText(R.string.no_search_items);
+        } else {
+            authorText.setText("");
+            titleText.setText(R.string.no_connection);
+        }
     }
 }
