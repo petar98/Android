e340424044ae3846fab5635e5629fa2601a2f2bc
petar98 <petaryanakiev.py@gmail.com>
2020-05-16 23:29:47 +0300

    added requestor class for performing http get requests

---
+++ ToRead/app/src/main/java/com/example/toread/requestor/NetworkUtils.java
@@ -0,0 +1,73 @@
+package com.example.toread.requestor;
+
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class NetworkUtils {
+    private static final String LOG_TAG = NetworkUtils.class.getSimpleName();
+
+    private static String requestUrl = null;
+
+    NetworkUtils(String url) {
+        this.requestUrl = url;
+    }
+
+    static String getBookInfo(String queryString) {
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+        String bookJSONString = null;
+
+        try {
+            Uri builtUri = Uri.parse(requestUrl).buildUpon()
+                    .build();
+
+            URL requestURL = new URL(builtUri.toString());
+
+            urlConnection = (HttpURLConnection) requestURL.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            InputStream inputStream = urlConnection.getInputStream();
+
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            StringBuilder builder = new StringBuilder();
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                builder.append(line);
+                builder.append("\n");
+            }
+
+            if (builder.length() == 0) {
+                return null;
+            }
+
+            bookJSONString = builder.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        Log.i(LOG_TAG, bookJSONString);
+        return bookJSONString;
+    }
+}
